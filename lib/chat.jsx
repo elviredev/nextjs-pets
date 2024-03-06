b63cba5b2011a32066355e6735f709aa116c65ae
elviredev <sandrine.rodriguez@hotmail.com>
2024-03-06 16:37:59 +0100

    Fixed trim issue on chat feature

--- lib/chat.jsx
+++ lib/chat.jsx
@@ -63,17 +63,17 @@ export default function Chat() {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({message: userMessage, socket_id: socketId})
+            body: JSON.stringify({ message: userMessage.trim(), socket_id: socketId })
         })
         // envoyer le message à notre copie locale de l'état
-        setMessageLog(prev => [...prev, {selfMessage: true, message: userMessage}])
+        setMessageLog(prev => [...prev, { selfMessage: true, message: userMessage.trim() }])
         // vider le champ input à chaque soulission du form
         setUserMessage("")
     }
 
     // Gérer le champ input
     function handleInputChange(e) {
-        setUserMessage(e.target.value.trim())
+        setUserMessage(e.target.value)
     }
 
     return (
